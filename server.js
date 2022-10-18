3886a726d6e057468fceb2ccb2f76940f1627645
RJ Dust Puppies <109449181+RosemaryJF@users.noreply.github.com>
2022-10-18 18:04:03 +1100

    Insertion of boiler plate code to server.js

---
+++ server.js
@@ -0,0 +1,150 @@
+const inquirer = require("inquirer");
+const express = require('express');
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connection to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'Arapiles052021.',
+        database: 'XXXXX_db'
+    },
+    console.log(`Connected to the XXXXX database.`)
+);
+
+// View all departments
+app.get('/api/departments', (req, res) => {
+    const sql = `SELECT id, movie_name AS title FROM movies`;
+    
+    db.query(sql, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+         return;
+      }
+      res.json({
+        message: 'success',
+        data: rows
+      });
+    });
+});
+
+// view all roles
+app.get('/api/roles', (req, res) => {
+    const sql = `SELECT id, movie_name AS title FROM movies`;
+    
+    db.query(sql, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+         return;
+      }
+      res.json({
+        message: 'success',
+        data: rows
+      });
+    });
+});
+  
+// View all employees
+app.get('/api/employees', (req, res) => {
+    const sql = `SELECT id, movie_name AS title FROM movies`;
+    
+    db.query(sql, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+         return;
+      }
+      res.json({
+        message: 'success',
+        data: rows
+      });
+    });
+});
+
+// Add a department
+app.post('/api/departments', ({ body }, res) => {
+    const sql = `INSERT INTO movies (movie_name)
+      VALUES (?)`;
+    const params = [body.movie_name];
+    
+    db.query(sql, params, (err, result) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: body
+      });
+    });
+});
+
+// Add a role
+app.post('/api/roles', ({ body }, res) => {
+    const sql = `INSERT INTO movies (movie_name)
+      VALUES (?)`;
+    const params = [body.movie_name];
+    
+    db.query(sql, params, (err, result) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: body
+      });
+    });
+});
+
+// Add an employee
+app.post('/api/employees', ({ body }, res) => {
+    const sql = `INSERT INTO movies (movie_name)
+      VALUES (?)`;
+    const params = [body.movie_name];
+    
+    db.query(sql, params, (err, result) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: body
+      });
+    });
+});
+
+// Update an employee role
+app.put('/api/employees/:id', (req, res) => {
+    const sql = `UPDATE reviews SET review = ? WHERE id = ?`;
+    const params = [req.body.review, req.params.id];
+  
+    db.query(sql, params, (err, result) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+      } else if (!result.affectedRows) {
+        res.json({
+          message: 'Movie not found'
+        });
+      } else {
+        res.json({
+          message: 'success',
+          data: req.body,
+          changes: result.affectedRows
+        });
+      }
+    });
+});
+  
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
