cac95d3fc66ed19a7f7b944a1e82c78ce4d50149
RJ Dust Puppies <109449181+RosemaryJF@users.noreply.github.com>
2022-10-25 11:52:15 +1100

    Addition of classes to api files

---
+++ routes/api/Employee.js
@@ -0,0 +1,13 @@
+const Department = require("./Department");
+
+class Employee extends Department {
+    super
+    constructor(first_name, last_name, role_id, manager_id) {
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.role_id = role_id;
+        this.manager_id = manager_id;
+    }
+}
+
+module.exports = Employee;
